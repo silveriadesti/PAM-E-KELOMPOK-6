61f49037de171965333fcc209e83a53a2984d84a
dyahcandradewi123 <dyahcandradewi@student.ub.ac.id>
2025-12-15 11:57:33 +0700

    nambahin supabase

---
+++ app/src/main/java/com/example/splashandregist/data/remote/SupabaseClient.kt
@@ -0,0 +1,17 @@
+
+// Gunakan 'object' agar dia jadi Singleton (Cuma ada 1 koneksi untuk seluruh aplikasi)
+object SupabaseClient {
+
+    private const val SUPABASE_URL = "https://yhuflxzqvednwqluokhz.supabase.co"
+    private const val SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlodWZseHpxdmVkbndxbHVva2h6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1ODMwNTEsImV4cCI6MjA4MTE1OTA1MX0.ePSA65T7DvGe4OvLT55AARtx4H-fsWlC-ISjYit3A7M"
+
+    val client = createSupabaseClient(
+        supabaseUrl = SUPABASE_URL,
+        supabaseKey = SUPABASE_KEY
+    ) {
+        // Pasang fitur-fitur yang mau dipakai
+        install(Auth.Companion)      // Buat Login
+        install(Postgrest.Companion) // Buat Database (CRUD)
+        install(Storage.Companion)   // Buat Upload Gambar
+    }
+}
